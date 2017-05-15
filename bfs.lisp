dd886c828648b1c4ceb1eb9ec4ad200fc62997ac
ricanontherun@gmail.com <ricanontherun@gmail.com>
2017-05-15 13:38:09 -0400

    Finished breadth-first search in lisp.

--- bfs.lisp
+++ bfs.lisp
@@ -1,51 +1,81 @@
 ; Prefilled adjacency list
 (defvar *graph* '(
 	(1 (2 3))
-	(2 (15 20))
+	(2 (15))
+	(15 (20))
 	(3 (100))
-))
+	))
 
+; Get the neighbors (adjacent vertices) to a vertex in the graph
 (defun get-neighbors(index)
 	(return-from get-neighbors (cadr (assoc index *graph*))))
 
+; Check if a particular vertex has been visited
 (defun vertex-visited(visited vertex)
 	(return-from vertex-visited
 		(not (eq (member vertex visited) nil))))
 
+; Check if a queue is empty
 (defun queue-empty(queue)
-  (return-from queue-empty (eq (length queue) 0) ))
+	(return-from queue-empty (eq (length queue) 0) ))
 
-(defun bfs(start)
+; Get the last element in a list
+(defun last-list-element(l)
+	(return-from last-list-element (nth (- (length l) 1) l)))
+
+; Perform a breadth-first search for 'goal' starting at 'start'
+(defun bfs(start goal)
   	; Initialize some local data structures
   	(setq visited '())
-	(setq queue '())
+  	(setq queue '())
+  	(setq found nil)
 
 	; Push the first vertex on the queue.
-	(push start queue)
+	(push (list start) queue)
 
 	(loop
-	  ; Get the next queue element
-	  (setq vertex (first queue))
-	  (format t "Processing vertex ~a ~%" vertex)
-
-	  ; Pop that element off the list.
+	  ; Pop the next processing path off of the queue
+	  (setq path (first queue))
 	  (setf queue (cdr queue))
 
-	  (setq neighbors (get-neighbors vertex))
+	  ; Get the last vertex in that path, we'll extend the frontier with this.
+	  (setq vertex (last-list-element path))
 
-	  (format t "Vertex ~a has neighbors ~a ~%" vertex neighbors)
+	  ; As soon as we find our goal state/node, break.
+	  (if (eq vertex goal)
+		(progn
+		  (setq found path)
+		  (return)))
+
+	  (setq neighbors (get-neighbors vertex))
 
 	  ; Loop over each neighbor
 	  (dolist (neighbor (get-neighbors vertex))
 		; if this neighbor hasn't already been visited
 		(if (eq (vertex-visited visited neighbor) nil)
-		  ; Mark it as visited and queue it up.
 		  (progn
+			; Extend the frontier by pushing a new path on the queue.
+			(setq queue
+				  (append queue
+						  (list (append path (list neighbor)))))
+
+			; Mark this vertex as visited
 			(push neighbor visited)
-			(push neighbor queue))))
+			)))
 
 	  ; Return as soon as the loop is empty
 	  (when (eq (queue-empty queue) t) (return nil)))
-	)
 
-(bfs 1)
+	(return-from bfs found))
+
+; Endless loop prompting the user to search for values in the graph above
+(loop
+  	(format t "Select a state to search for: ")
+	(setq goal (read))
+
+	(setq path (bfs 1 goal))
+
+	(if (not (eq path nil))
+	  (format t "Found ~a in path ~a ~%" goal path)
+	  (format t "Sorry, couldn't find ~a~%" goal))
+  )
