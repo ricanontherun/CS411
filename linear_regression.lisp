5e5f682ece6e520e3c65adc3f19af171617383fd
ricanontherun@gmail.com <ricanontherun@gmail.com>
2017-05-28 11:58:42 -0400

    Implemented the coefficients of the linear regression alg

--- linear_regression.lisp
+++ linear_regression.lisp
@@ -1,30 +1,22 @@
-; Define a struct 'point' which contains two members, x and y
-(defstruct point x y)
-
-; Make a list of point structs, this is our data set.
-(setq *points* (list
-	(make-point :x 10 :y 2)
-	(make-point :x 20 :y 200)))
-
 ; Calculate the mean of a list of values
 ; Returns either the mean of the values, or -1 on error.
-(defun mean(values)
+(defun Mean(values)
   (setq num (length values))
   (setq value 0)
 
   (if (eq num 0)
-	(return-from mean -1))
+	(return-from Mean -1))
 
   (dolist (v values)
 	(setq value (+ value v)))
 
-  (return-from mean (/ value num)))
+  (return-from Mean (/ value num)))
 
 ; Calculate the variance of a set of values.
 ; Returns either the variance of the values, or -1 on error.
-(defun variance (values)
+(defun Variance (values)
   (if (eq (length values) 0)
-	(return-from variance -1))
+	(return-from Variance -1))
 
   (setq mean (mean values))
   (setq squared_differences ())
@@ -33,39 +25,48 @@
   (dolist (v values)
 	(push (expt (- mean v) 2) squared_differences))
 
-  ; Return the mean of those squared differences 
-  (return-from variance (mean squared_differences)))
+  ; Return the mean of those squared differences
+  (return-from Variance (mean squared_differences)))
 
-; Calculate the covariance for a set of points
+; Calculate the covariance for a set of data
 ; Returns the covariance or -1 on error
-(defun covariance(points)
-  	(setq num_points (length points))
+(defun Covariance(x y)
+  	(setq len (length x))
 
-	; We need at least two points to calculate covariance.
-  	(if (< num_points 2)
-	  (return-from covariance -1))
+	; We need at least two points to calculate Covariance.
+  	(if (< len 2)
+	  (return-from Covariance -1))
 
-	; Extract the x values from the points
-	(setq x_data ())
-
-	(dolist (point *points*)
-	  (push (point-x point) x_data))
-
-	; Extract the y values from the points
-	(setq y_data ())
-
-	(dolist (point *points*)
-	  (push (point-y point) y_data))
-	
-	(setq x_mean (mean x_data))
-	(setq y_mean (mean y_data))
+	(setq x_mean (mean x))
+	(setq y_mean (mean y))
 	(setq sum 0)
-	
-	(dolist (point points)
-	  (setq sum (+ sum
-		(* (- (point-x point) x_mean) (- (point-y point) y_mean))
-		)))
-	
-	(return-from covariance (/ sum num_points)))
 
-(format t "Covariance of ~d is ~f ~%" *points* (covariance *points*))
+	(loop
+	  for i from 0 to (- len 1)
+	  do (setq sum (+ sum
+		(* (- (nth i x) x_mean) (- (nth i y) y_mean))
+		)))
+
+	(return-from Covariance (/ sum len)))
+
+(defun CalculateCoefficients(x y)
+  (setq covariance (Covariance x y))
+  (setq variance (Variance x))
+  (setq b1 (/ covariance variance))
+
+  (setq x_mean (Mean x))
+  (setq y_mean (Mean y))
+
+  (setq b0 (- y_mean (* b1 x_mean)))
+
+  (return-from CalculateCoefficients (list b0 b1)))
+
+; TESTING SITUATION
+; Let's say I consistently pay 50% tips on all means. Let's predict what my
+; next tip amount should be!
+(setq x (list 10 20 30 40))
+(setq y (list 5 10 15 20))
+
+(setq coefficients (CalculateCoefficients x y))
+
+(format t "~f ~f ~%" (nth 0 coefficients) (nth 1 coefficients))
